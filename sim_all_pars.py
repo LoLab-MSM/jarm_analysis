9e33ceb528d2dfe62b78e037b30a4d52ef5120d1
oscar <oo.ortega2247@gmail.com>
2018-07-19 13:33:05 -0500

    updated pydream parameters

--- sim_all_pars.py
+++ sim_all_pars.py
@@ -6,11 +6,11 @@ from pysb.simulator import CupSodaSimulator
 tspan = np.linspace(0, 60, 60)
 tspan_eq = np.linspace(0, 30, 30)
 
-chain0 = np.load('calibration_normalization_preequilibration/jnk3_dreamzs_5chain_sampled_params_chain_0_50000.npy')
-chain1 = np.load('calibration_normalization_preequilibration/jnk3_dreamzs_5chain_sampled_params_chain_1_50000.npy')
-chain2 = np.load('calibration_normalization_preequilibration/jnk3_dreamzs_5chain_sampled_params_chain_2_50000.npy')
-chain3 = np.load('calibration_normalization_preequilibration/jnk3_dreamzs_5chain_sampled_params_chain_3_50000.npy')
-chain4 = np.load('calibration_normalization_preequilibration/jnk3_dreamzs_5chain_sampled_params_chain_4_50000.npy')
+chain0 = np.load('calibration_normalization_preequilibration/jnk3_dreamzs_5chain_sampled_params_chain_0_200000.npy')
+chain1 = np.load('calibration_normalization_preequilibration/jnk3_dreamzs_5chain_sampled_params_chain_1_200000.npy')
+chain2 = np.load('calibration_normalization_preequilibration/jnk3_dreamzs_5chain_sampled_params_chain_2_200000.npy')
+chain3 = np.load('calibration_normalization_preequilibration/jnk3_dreamzs_5chain_sampled_params_chain_3_200000.npy')
+chain4 = np.load('calibration_normalization_preequilibration/jnk3_dreamzs_5chain_sampled_params_chain_4_200000.npy')
 
 total_iterations = chain0.shape[0]
 burnin = int(total_iterations / 2)
@@ -18,7 +18,7 @@ print (burnin)
 samples = np.concatenate((chain0[burnin:, :], chain1[burnin:, :], chain2[burnin:, :],
                           chain3[burnin:, :], chain4[burnin:, :]))
 
-idx_pars_calibrate = [1, 15, 17, 19, 24, 25, 26, 27]
+idx_pars_calibrate = [1, 5, 9, 11, 15, 17, 23, 25, 27, 29, 33, 37, 38, 39, 40, 41, 43, 45]
 rates_of_interest_mask = [i in idx_pars_calibrate for i, par in enumerate(model.parameters)]
 param_values = np.array([p.value for p in model.parameters])
 
@@ -32,8 +32,10 @@ integrator_opt = {'rtol': 1e-6, 'atol': 1e-6, 'mxsteps': 20000}
 cupsoda_solver = CupSodaSimulator(model, tspan=tspan_eq, gpu=0, obs_species_only=False,
                                   memory_usage='shared_constant', integrator_options=integrator_opt)
 
+kcat_idx = [38, 39]
+
 pars_eq = np.copy(all_parameters)
-pars_eq[:, [24, 25]] = 0
+pars_eq[:, kcat_idx] = 0
 conc_eq = pre_equilibration(cupsoda_solver, param_values=pars_eq)[1]
 
 sims_final = CupSodaSimulator(model, tspan=tspan, gpu=0, obs_species_only=False,
