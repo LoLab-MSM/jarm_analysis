2cbf04471262f27a06a361b549c88ea230c32d71
oscar <oo.ortega2247@gmail.com>
2018-07-19 13:07:34 -0500

    run tropical with different initial conditions

--- run_tropical.py
+++ run_tropical.py
@@ -2,7 +2,7 @@ import pickle
 from tropical.dynamic_signatures_range import run_tropical_multi
 from jnk3_no_ask1 import model
 
-a = run_tropical_multi(model, simulations='all_simulations_jnk3.h5', cpu_cores=30, verbose=True)
+a = run_tropical_multi(model, simulations='simulations_ic_jnk3.h5', cpu_cores=30, add_observables=True, verbose=True)
 
-with open('pydream_kpars_signatures.pickle', 'wb') as handle:
+with open('ic_obs_signatures.pickle', 'wb') as handle:
     pickle.dump(a, handle, protocol=pickle.HIGHEST_PROTOCOL)
