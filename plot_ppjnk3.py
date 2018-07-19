8a7cf25bfbd454a7030d90e9be510b6f4b6d6243
oscar <oo.ortega2247@gmail.com>
2018-07-19 13:09:05 -0500

    plot different initial conditions simulations

--- plot_ppjnk3.py
+++ plot_ppjnk3.py
@@ -1,8 +1,14 @@
 from tropical.cluster_analysis import AnalysisCluster
 from jnk3_no_ask1 import model
+import numpy as np
 
-simulations = 'all_simulations_jnk3.h5'
+simulations = 'simulations_ic_jnk3_oneP.h5'
+labels = np.load('ic_labels_pro1p_alljnk3.npy')
 
-a = AnalysisCluster(model, clusters=None, sim_results=simulations)
+a = AnalysisCluster(model, sim_results=simulations, clusters=labels)
 
-a.plot_dynamics_cluster_types([27], norm=False, fig_label='ppJNK3')
+a.hist_avg_sps(model.monomers['JNK3'], fig_name='JNK3')
+#a.plot_dynamics_cluster_types([model.reactions_bidirectional[26]['rate']], norm=False)
+#a.plot_sp_ic_overlap([32])
+
+
