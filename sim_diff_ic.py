3c940d4ec70a796f53b728995c3c3f99223f87b8
oscar <oo.ortega2247@gmail.com>
2018-07-30 20:59:08 -0500

    Updated model and parameters

--- sim_diff_ic.py
+++ sim_diff_ic.py
@@ -3,10 +3,10 @@ import numpy as np
 from equilibration_function import pre_equilibration
 from pysb.simulator import CupSodaSimulator
 
-idx_pars_calibrate = [1, 5, 9, 11, 15, 17, 23, 25, 27, 29, 33, 37, 38, 39, 40, 41, 43, 45]
+idx_pars_calibrate = [5, 9, 11, 15, 17, 23, 25, 27, 31, 35, 36, 37, 38, 39, 41, 43] #pydream2
 rates_of_interest_mask = [i in idx_pars_calibrate for i, par in enumerate(model.parameters)]
 
-calibrated_pars = np.load('pydream_most_likely_200000.npy')
+calibrated_pars = np.load('most_likely_par_100000_2.npy')
 param_values = np.array([p.value for p in model.parameters])
 
 par_set_calibrated = np.copy(param_values)
@@ -19,18 +19,18 @@ max_arrestin = 100
 arrestin_initials = np.linspace(0, max_arrestin, n_conditions)
 par_clus1 = par_set_calibrated
 
-arrestin_idx = 46
+arrestin_idx = 44
+kcat_idx = [36, 37]
 repeated_parameter_values = np.tile(par_clus1, (n_conditions, 1))
 repeated_parameter_values[:, arrestin_idx] = arrestin_initials
 np.save('arrestin_diff_IC_par0.npy', repeated_parameter_values)
 
-tspan_eq = np.linspace(0, 30, 30)
+tspan_eq = np.linspace(0, 1000, 100)
 integrator_opt = {'rtol': 1e-6, 'atol': 1e-6, 'mxsteps': 20000}
 
 cupsoda_solver = CupSodaSimulator(model, tspan=tspan_eq, gpu=0, obs_species_only=False,
                                   memory_usage='shared_constant', integrator_options=integrator_opt)
 
-kcat_idx = [38, 39]
 pars_eq = np.copy(repeated_parameter_values)
 pars_eq[:, kcat_idx] = 0
 conc_eq = pre_equilibration(cupsoda_solver, param_values=pars_eq)[1]
