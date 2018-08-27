f0379f0c98294beb9bce00f866544fc0c2ea16b4
oscar <oo.ortega2247@gmail.com>
2018-08-27 12:04:22 -0500

    more plots

--- path_flux_analysis_ic.py
+++ path_flux_analysis_ic.py
@@ -1,11 +1,12 @@
 from tropical.dominant_path_analysis import run_dompath_multi
 from tropical.clustering import ClusterSequences
 from tropical.plot_signatures import PlotSequences
+from tropical.cluster_analysis import AnalysisCluster
 from jnk3_no_ask1 import model
 import numpy as np
 import pickle
 
-d = run_dompath_multi(model, 'simulations_ic_jnk3.h5', target='s27', depth=4, cpu_cores=20)
+d = run_dompath_multi(model, 'simulations_ic_jnk3.h5', target='s27', depth=5, cpu_cores=20)
 path_signatures = d['signatures']
 np.save('dom_path_signatures.npy', path_signatures)
 
@@ -24,4 +25,20 @@ ps = PlotSequences(cs)
 ps.plot_sequences(type_fig='modal', title='modal_path')
 ps.plot_sequences(type_fig='trajectories', title='trajectories_path')
 ps.plot_sequences(type_fig='entropy', title='modal_path')
+
+ac =AnalysisCluster(model, sim_results='simulations_ic_jnk3.h5', clusters=cs.labels)
+ac.plot_cluster_dynamics([27])
+
+jnk3 = model.monomers['JNK3']
+mkk4 = model.monomers['MKK4']
+mkk7 = model.monomers['MKK7']
+ac.plot_pattern_sps_distribution(pattern=jnk3, type_fig='bar', fig_name='jnk3_bar')
+ac.plot_pattern_sps_distribution(pattern=jnk3, type_fig='entropy', fig_name='entropy')
+ac.plot_pattern_sps_distribution(pattern=mkk4, type_fig='bar', fig_name='mkk4_bar')
+ac.plot_pattern_sps_distribution(pattern=mkk4, type_fig='entropy', fig_name='mkk4_entropy')
+ac.plot_pattern_sps_distribution(pattern=mkk7, type_fig='bar', fig_name='mkk7_bar')
+ac.plot_pattern_sps_distribution(pattern=mkk7, type_fig='entropy', fig_name='mkk4_entropy')
+
+
+
 
