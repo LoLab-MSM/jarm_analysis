23349a787779955778631e97f6ef4d9cbac0977c
oscar ortega <oscar@diablo.lolab.xyz>
2018-09-10 15:08:16 -0500

    use pattern name for image name

--- run_tropical.py
+++ run_tropical.py
@@ -2,18 +2,26 @@ from tropical.pydrone import Pydrone
 from jnk3_no_ask1 import model
 
 pyd = Pydrone(model, 'simulations_ic_jnk3.h5', 1)
+print('loaded simulations')
 pyd.discretize(cpu_cores=20)
-pyd.cluster_signatures_spectral(species='__s2_p', nclusters=10, cluster_range=True, cpu_cores=20)
+print('Discretized trajectories')
+pyd.cluster_signatures_spectral(species='__s1_c', nclusters=10, cluster_range=True, cpu_cores=20)
+print('clustered trajectories')
 ac = pyd.analysis_cluster
 ps = pyd.plot_signatures
-ac.plot_cluster_dynamics(27, fig_name='27')
+
+print('Plotting')
+ac.plot_cluster_dynamics([1], fig_name='1', norm=True, norm_value=0.05)
 
 # jnk3 monomer
 jnk3 = model.monomers['JNK3']
-ac.plot_pattern_rxns_distribution(jnk3, type_fig='bar', fig_name='jnk3')
-ac.plot_pattern_rxns_distribution(jnk3, type_fig='entropy', fig_name='jnk3')
-ac.plot_pattern_sps_distribution(jnk3, type_fig='bar', fig_name='jnk3')
-ac.plot_pattern_sps_distribution(jnk3, type_fig='entropy', fig_name='jnk3')
+mkk4 = model.monomers['MKK4']
+pattern = mkk4
 
-ps.plot_sequences(type_fig='modal', title='jnk3')
-ps.plot_sequences(type_fig='trajectories', title=jnk3)
+ac.plot_pattern_rxns_distribution(pattern, type_fig='bar', fig_name='bar_{0}'.format(pattern.name))
+ac.plot_pattern_rxns_distribution(pattern, type_fig='entropy', fig_name='ent_{0}'.format(pattern.name))
+ac.plot_pattern_sps_distribution(pattern, type_fig='bar', fig_name='bar_{0}'.format(pattern.name))
+ac.plot_pattern_sps_distribution(pattern, type_fig='entropy', fig_name='ent_{0}'.format(pattern.name))
+
+ps.plot_sequences(type_fig='modal', title='modal')
+ps.plot_sequences(type_fig='trajectories', title='trajectories')
