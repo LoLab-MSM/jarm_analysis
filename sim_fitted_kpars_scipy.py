3f4fcb01c7e3b9a5d994b8627070aab392644bb8
oscar <oo.ortega2247@gmail.com>
2018-10-15 17:02:09 -0500

    make tspan same as of the experimental data

--- sim_fitted_kpars_scipy.py
+++ sim_fitted_kpars_scipy.py
@@ -3,11 +3,11 @@ import numpy as np
 from equilibration_function import pre_equilibration
 from pysb.simulator import ScipyOdeSimulator
 
-chain0 = np.load('pydream_results_test1/jnk3_dreamzs_5chain_sampled_params_chain_0_500000.npy')
-chain1 = np.load('pydream_results_test1/jnk3_dreamzs_5chain_sampled_params_chain_1_500000.npy')
-chain2 = np.load('pydream_results_test1/jnk3_dreamzs_5chain_sampled_params_chain_2_500000.npy')
-chain3 = np.load('pydream_results_test1/jnk3_dreamzs_5chain_sampled_params_chain_3_500000.npy')
-chain4 = np.load('pydream_results_test1/jnk3_dreamzs_5chain_sampled_params_chain_4_500000.npy')
+chain0 = np.load('pydream_results_test/jnk3_dreamzs_5chain_sampled_params_chain_0_500000.npy')
+chain1 = np.load('pydream_results_test/jnk3_dreamzs_5chain_sampled_params_chain_1_500000.npy')
+chain2 = np.load('pydream_results_test/jnk3_dreamzs_5chain_sampled_params_chain_2_500000.npy')
+chain3 = np.load('pydream_results_test/jnk3_dreamzs_5chain_sampled_params_chain_3_500000.npy')
+chain4 = np.load('pydream_results_test/jnk3_dreamzs_5chain_sampled_params_chain_4_500000.npy')
 
 total_iterations = chain0.shape[0]
 burnin = int(total_iterations / 2)
@@ -18,18 +18,18 @@ idx_pars_calibrate = [1, 5, 9, 11, 15, 17, 19, 23, 25, 27, 31, 35, 36, 37, 38, 3
 
 # Save most likely parameter
 unique_pars, indices, counts = np.unique(samples, return_index=True, return_counts=True, axis=0)
-max_idx = np.argsort(counts)[::-1]
+counts_sorted = np.argsort(counts)[::-1]
 
 rates_of_interest_mask = [i in idx_pars_calibrate for i, par in enumerate(model.parameters)]
 
 param_values = np.array([p.value for p in model.parameters])
 
-unique_pars = unique_pars[max_idx[:5000]]
+unique_pars = unique_pars[counts_sorted[:5000]]
 par_set_calibrated = np.copy(param_values)
 all_par_set_calibrated = np.tile(par_set_calibrated, (len(unique_pars), 1))
 all_par_set_calibrated[:, rates_of_interest_mask] = 10 ** unique_pars
 
-tspan = np.linspace(0, 180, 121)
+tspan = np.linspace(0, 180, 181)
 
 arrestin_idx = 44
 kcat_idx = [36, 37]
@@ -45,7 +45,7 @@ conc_eq = pre_equilibration(scipy_solver, param_values=pars_eq)[1]
 sims_final = ScipyOdeSimulator(model, tspan=tspan).run(param_values=all_par_set_calibrated,
                                                        initials=conc_eq)
 print(sims_final.dataframe['__s27'])
-sims_final.save('simulations/simulations_arrestin_jnk3_scipy.h5')
+sims_final.save('simulations/simulations_arrestin_scipy_181.h5')
 
 # Simulations without arrestin
 jnk3_initial_idxs = [47, 48, 49]
@@ -62,4 +62,4 @@ conc_eq_noarrestin = pre_equilibration(scipy_solver, param_values=pars_eq_noarre
 sims_final_noarrestin = ScipyOdeSimulator(model, tspan=tspan).run(param_values=all_pars_noarrestin,
                                                                   initials=conc_eq_noarrestin)
 print(sims_final_noarrestin.dataframe['__s27'])
-sims_final_noarrestin.save('simulations/simulations_noarrestin_jnk3_scipy.h5')
+sims_final_noarrestin.save('simulations/simulations_noarrestin_scipy_181.h5')
