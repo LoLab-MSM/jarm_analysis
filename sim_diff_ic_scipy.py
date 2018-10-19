44f565a0484315aa041c414a920f43125784afa6
oscar <oo.ortega2247@gmail.com>
2018-10-19 13:24:06 -0500

    updated time points of the simulations

--- sim_diff_ic_scipy.py
+++ sim_diff_ic_scipy.py
@@ -7,13 +7,13 @@ idx_pars_calibrate = [1, 5, 9, 11, 15, 17, 19, 23, 25,
                       27, 31, 35, 36, 37, 38, 39, 41, 43] #pydream3
 rates_of_interest_mask = [i in idx_pars_calibrate for i, par in enumerate(model.parameters)]
 
-calibrated_pars = np.load('most_likely_par_500000_box.npy')
+calibrated_pars = np.load('most_likely_par_500000_4box.npy')
 param_values = np.array([p.value for p in model.parameters])
 
 par_set_calibrated = np.copy(param_values)
 par_set_calibrated[rates_of_interest_mask] = 10 ** calibrated_pars
 
-tspan = np.linspace(0, 180, 121)
+tspan = np.linspace(0, 180, 181)
 
 n_conditions = 5000
 max_arrestin = 100
@@ -37,4 +37,4 @@ conc_eq = pre_equilibration(scipy_solver, param_values=pars_eq)[1]
 sims_final = ScipyOdeSimulator(model, tspan=tspan).run(param_values=repeated_parameter_values,
                                                        initials=conc_eq)
 
-sims_final.save('simulations_ic_scipy_box.h5')
+sims_final.save('simulations_ic_scipy_181.h5')
